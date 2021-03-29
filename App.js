ce86f490dc8fa530ddb916b3eafb5b5713ab4247
Thomas Gillet <Gillet.thomas43@gmail.com>
2021-03-29 09:04:30 +0200

    Create about/contact screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import Navigation from "./app/config/Navigation";
+import AboutScreen from "./app/screens/AboutScreen";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Navigation />
+    // </NavigationContainer>
+    <AboutScreen></AboutScreen>
   );
 }
