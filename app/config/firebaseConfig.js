64ab16b4c22830c6d5bb2b9f27f6cdd78b8d9f67
Thomas Gillet <Gillet.thomas43@gmail.com>
2021-03-30 10:56:25 +0200

    Connect MainScreen with DB

--- app/config/firebaseConfig.js
+++ app/config/firebaseConfig.js
@@ -1,19 +1,21 @@
-import * as firebase from 'firebase';
-import '@firebase/auth';
-import '@firebase/firestore';
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCZU4d0ucoP96juIZGmJ-5yyk8DB19yMgQ',
-  authDomain: 'se-project-b5240.firebaseapp.com',
-  databaseURL: 'https://se-project-b5240.firebaseio.com',
-  projectId: 'se-project-b5240',
-  storageBucket: 'se-project-b5240.appspot.com',
-  messagingSenderId: '530520127403',
-  appId: '1:530520127403:android:912bf179932a7c0e6d44ee',
+  apiKey: "AIzaSyC3FlCanXwwVTRHgPzAJtOpJmUnMzxJOvk",
+  authDomain: "se-project-b5240.firebaseapp.com",
+  databaseURL: "https://se-project-b5240-default-rtdb.firebaseio.com",
+  projectId: "se-project-b5240",
+  storageBucket: "se-project-b5240.appspot.com",
+  messagingSenderId: "530520127403",
+  appId: "1:530520127403:web:ba3eefed9b1785406d44ee",
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
 }
 
-export { firebase };
+export default firebase;
--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+import firebase from "../config/firebaseConfig";
+
 class MainScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    firebase
+      .database()
+      .ref()
+      .child("Waypoint")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          var data = [];
+          snapshot.forEach((entry) => {
+            data.push({
+              key: entry.key,
+              value: entry.value,
+            });
+          });
+          console.log(data);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text> MainScreen </Text>
       </View>
     );
