952a93135993bf1d35eea38229c24c1ecaaf00d4
COSSA <hugo.cossa@hotmail.fr>
2021-05-09 11:53:07 +0200

    Create quiz inside modaf for waypoints

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,92 +1,221 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Modal } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+  Modal,
+  Button,
+} from "react-native";
 
 class MainScreen extends Component {
-  //Initiating the state of the modal
   state = {
-    isVisible: false
+    isVisible: false,
+    waypoints: {
+      category: "Science",
+      answers: [null, "500", "1000", "1500"],
+      correctAnswer: 1,
+      information: "MSA has 20 classrooms",
+      question: "Number of floors in MSA ?",
+      title: "MSA",
+    },
+    answered: false,
   };
 
-  // If we want to hide the --> Show modal
-  displayModal(show){
-    this.setState({ isVisible: show })
+  checkValidity(userInput) {
+    if (this.state.waypoints != null) {
+      var correctAnswer = this.state.waypoints.correctAnswer;
+      if (this.state.waypoints.answers[correctAnswer] === userInput)
+        return true;
+      return false;
+    } else {
+      return null;
+    }
+  }
+
+  displayModal(show) {
+    this.setState({ isVisible: show });
   }
 
   render() {
     return (
-      <View style = {styles.container}>
-        <Modal 
-          animationType = {"slide"}
-          transparent = {false}
-          visible = {this.state.isVisible} >
+      <View style={styles.container}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.isVisible}
+        >
+          <Image
+            source={require("../assets/BuildingBelval.jpg")}
+            style={styles.image}
+          />
 
-            <Image 
-              source = {require('../assets/BuildingBelval.jpg')}
-              style = {styles.image}/>
+          <Text style={styles.information}>
+            {this.state.waypoints.information.toUpperCase()}
+          </Text>
 
-              <Text style = {styles.text}>
-                This is the pop up we are going to show, with explanation about building.
-                 Maybe we could implement the quizz here?</Text>
+          <TouchableHighlight
+            activeOpacity={0.9}
+            style={styles.appButtonContainer}
+          >
+            <Text style={styles.appButtonText}>
+              {this.state.waypoints.question}
+            </Text>
+          </TouchableHighlight>
 
-              <Text style = {styles.closeText}
-              onPress = {()=> {
-                this.displayModal(!this.state.isVisible);}}>Close Modal</Text>
-          </Modal>
-
-        <TouchableOpacity style = {styles.button}
-          onPress = {()=> {
-            this.displayModal(true)
-          }}>
-            <Text style = {styles.buttonText}>WAYPOINTS</Text>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.buttonAnswer}
+            onPress={() => {
+              this.checkValidity (this.state.waypoints.answers[1]);
+            }}
+          >
+            <Text style={styles.appButtonTextAnswer}>
+              {this.state.waypoints.answers[1]}
+            </Text>
           </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.buttonAnswer}
+            onPress={() => {
+              this.checkValidity(this.state.waypoints.answers[2]);
+            }}
+          >
+            <Text style={styles.appButtonTextAnswer}>
+              {this.state.waypoints.answers[2]}
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.buttonAnswer}
+            onPress={() => {
+              this.checkValidity(this.state.waypoints.answers[3]);
+            }}
+          >
+            <Text style={styles.appButtonTextAnswer}>
+              {this.state.waypoints.answers[3]}
+            </Text>
+          </TouchableOpacity>
+
+          <Text
+            style={styles.closeText}
+            onPress={() => {
+              this.displayModal(!this.state.isVisible);
+            }}
+          >
+            {" "}
+            Exit{" "}
+          </Text>
+        </Modal>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            this.displayModal(true);
+          }}
+        >
+          <Text style={styles.buttonText}>WAYPOINTS</Text>
+        </TouchableOpacity>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    padding: 80,
+    padding: 50,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    display: 'flex',
+    display: "flex",
     height: 50,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: '#2278FF',
-    shadowColor: '#2278FF',
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#2278FF",
+    shadowColor: "#2278FF",
     shadowOpacity: 0.5,
-    shadowOffset: { 
-      height: 10, 
-      width: 0
+    shadowOffset: {
+      height: 10,
+      width: 0,
     },
-  shadowRadius: 25
+    shadowRadius: 25,
   },
   buttonText: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 25,
   },
   image: {
-    marginTop: '30%',
-    marginBottom: '5%',
-    width: '100%',
-    height: '35%',
+    marginTop: "20%",
+    marginBottom: -20,
+    width: "100%",
+    height: "35%",
   },
-  text: {
+  information: {
+    fontSize: 25,
+    marginBottom: "13%",
+    padding: 50,
+    marginTop: 1,
+    marginLeft: 16,
+    fontWeight: "bold",
+  },
+  question: {
     fontSize: 24,
-    marginBottom: 30,
+    marginBottom: "0%",
     padding: 40,
   },
   closeText: {
-    fontSize: 24,
-    color: '#00479e',
-    textAlign: 'center',
-  }
+    fontSize: 34,
+    color: "#00479e",
+    textAlign: "center",
+    marginTop: "10%",
+  },
+  appButtonContainer: {
+    elevation: 30,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    textAlign: "center",
+    padding: 30,
+    marginTop: -80,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  buttonAnswer: {
+    elevation: 30,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    textAlign: "center",
+    padding: 30,
+    marginTop: 20,
+    marginLeft: 100,
+    marginRight: 100,
+    borderColor: "black",
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+  },
+  appButtonTextAnswer: {
+    fontSize: 20,
+    color: "#000000",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+  },
 });
 
 export default MainScreen;
