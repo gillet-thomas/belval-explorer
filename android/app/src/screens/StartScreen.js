edf913fc1ad59f616a48db3769c1161ea30735e1
Gilles <gilchen96@gmail.com>
2021-05-19 12:30:30 +0200

    Adjust Logo Position

--- android/app/src/screens/StartScreen.js
+++ android/app/src/screens/StartScreen.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   ImageBackground,
   Image,
   Text,
   Pressable,
-} from "react-native";
+} from 'react-native';
 
 import firebase from '../config/firebaseConfig';
-import config from "../config/config";
+import config from '../config/config';
 
 class StartScreen extends Component {
   state = {
     quote: null,
-  }
+  };
   componentDidMount() {
     firebase
       .database()
@@ -23,26 +23,25 @@ class StartScreen extends Component {
       .then(snapshot => {
         if (snapshot.exists()) {
           var data = [];
-          snapshot.forEach(entry => { data.push(entry) });
+          snapshot.forEach(entry => {
+            data.push(entry);
+          });
           const quotesNumber = data.length;
           const randomQuote = Math.floor(Math.random() * quotesNumber);
-          this.setState({ quote: JSON.stringify(data[randomQuote]) });
+          this.setState({quote: JSON.stringify(data[randomQuote])});
         }
       });
   }
 
   render() {
-    const { navigation } = this.props;
+    const {navigation} = this.props;
     return (
       <ImageBackground
         style={styles.background}
-        source={require("../assets/MainScreenBackground.jpg")}
-      >
+        source={require('../assets/MainScreenBackground.jpg')}>
         <Image
           style={styles.logo}
-          source={require("../assets/logo_white.png")}
-        ></Image>
-
+          source={require('../assets/logo_white.png')}></Image>
 
         {/* When start button pressed, remove this page from stack navigator and go to main page */}
         <Pressable
@@ -51,14 +50,13 @@ class StartScreen extends Component {
               index: 0,
               routes: [
                 {
-                  name: "Main",
+                  name: 'Main',
                 },
               ],
             })
           }
           style={styles.button}
-          android_ripple={{ color: config.START_BUTTON_RIPPLE_COLOR }}
-        >
+          android_ripple={{color: config.START_BUTTON_RIPPLE_COLOR}}>
           <Text style={styles.textButton}>Explore</Text>
         </Pressable>
         <Text style={styles.quote}>{this.state.quote}</Text>
@@ -70,42 +68,42 @@ class StartScreen extends Component {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    alignContent: "center",
-    width: "100%",
+    alignContent: 'center',
+    width: '100%',
   },
   button: {
     backgroundColor: config.START_BUTTON_BG_COLOR,
     borderRadius: 20,
-    position: "absolute",
-    alignSelf: "center",
-    width: "50%",
-    bottom: "16%",
+    position: 'absolute',
+    alignSelf: 'center',
+    width: '50%',
+    bottom: '16%',
     padding: 20,
     marginTop: 10,
     marginBottom: 10,
     borderWidth: 2,
-    borderColor: "#ffff",
+    borderColor: '#ffff',
   },
   textButton: {
-    alignSelf: "center",
-    color: "#ffff",
+    alignSelf: 'center',
+    color: '#ffff',
     fontSize: 30,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   quote: {
-    position: "absolute",
-    alignSelf: "center",
-    bottom: "40%",
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: '40%',
     padding: 20,
-    color: "white",
-    fontSize: 40,
+    color: 'white',
+    fontSize: 35,
   },
   logo: {
-    position: "absolute",
-    alignSelf: "center",
-    height: "35%",
-    top: -40
-  }
+    position: 'absolute',
+    alignSelf: 'center',
+    height: '30%',
+    top: -45,
+  },
 });
 
 export default StartScreen;
